d6e33af54d4f4a618953c9ae093c6c888665e300
jiyoon0114 <greenpkh02@naver.com>
2026-08-25 14:53:02 +0900

    20260825 | chore: 초기 환경 설정

---
+++ src/main/java/com/zipdamember/ZipDaMemberApplication.java
@@ -0,0 +1,13 @@
+package com.zipdamember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipDaMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipDaMemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zipdamember/ZipDaMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipdamember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipDaMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
