bd3d55b05bebb30c09352a19a306f5e973019ea1
jiyoon0114 <greenpkh02@naver.com>
2026-08-27 19:08:24 +0900

    20260827 | chore: 의존성, 야물, main 클래스 어노테이션 추가

--- src/main/java/com/zipdamember/ZipDaMemberApplication.java
+++ src/main/java/com/zipdamember/ZipDaMemberApplication.java
@@ -2,8 +2,12 @@ package com.zipdamember;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class ZipDaMemberApplication {
 
     public static void main(String[] args) {
