43c351af3cfc17b56498f7b9d5e57269eb3098b7
jiyoon0114 <greenpkh02@naver.com>
2026-08-28 19:00:24 +0900

    20260828 | feat: MissingServletRequestParameterException 글로벌 에러 핸들러에 추가

--- src/main/java/com/zipdamember/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/zipdamember/global/error/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -54,10 +55,23 @@ public class GlobalExceptionHandler {
         return this.generateErrorResponse(CustomResponseCode.UNAUTHORIZED_ERROR);
     }
 
+    // 메서드 파라미터가 맞지 않음
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<GlobalResponseDTO<Void>> handle(MethodArgumentTypeMismatchException e) {
         log.debug(CustomResponseCode.INVALID_PARAMETER_ERROR.name(), String.format("%s : 필드를 확인해 주세요.", e.getName()));
         return this.generateErrorResponse(CustomResponseCode.INVALID_PARAMETER_ERROR);
+    }
+
+    // 메서드 파라미터가 없음
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<GlobalResponseDTO<Void>> handle(
+            MissingServletRequestParameterException e
+    ) {
+        log.debug("필수 요청 파라미터 누락: {}", e.getParameterName());
+
+        return generateErrorResponse(
+                CustomResponseCode.INVALID_PARAMETER_ERROR
+        );
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
